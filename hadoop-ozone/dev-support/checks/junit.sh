10e1ea894a99b2455511022aef35dd5c2e069ce9
Doroszlai, Attila <adoroszlai@apache.org>
2023-02-19 13:26:56 +0100

    Stop iterations on error if FAIL_FAST==true

--- hadoop-ozone/dev-support/checks/junit.sh
+++ hadoop-ozone/dev-support/checks/junit.sh
@@ -69,6 +69,10 @@ for i in $(seq 1 ${ITERATIONS}); do
   if [[ ${rc} == 0 ]]; then
     rc=${irc}
   fi
+
+  if [[ "${FAIL_FAST:-}" == "true" ]] && [[ ${rc} != 0 ]]; then
+    break
+  fi
 done
 
 #Archive combined jacoco records
