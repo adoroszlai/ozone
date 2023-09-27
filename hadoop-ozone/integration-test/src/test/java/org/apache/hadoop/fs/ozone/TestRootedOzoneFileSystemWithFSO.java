f2e372ad8b2f3ca322acd66058b9ff141705bb83
Doroszlai, Attila <adoroszlai@apache.org>
2023-09-27 17:53:03 +0200

    disable testLeaseRecoverable

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/TestRootedOzoneFileSystemWithFSO.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/fs/ozone/TestRootedOzoneFileSystemWithFSO.java
@@ -28,6 +28,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
@@ -271,6 +272,7 @@ public class TestRootedOzoneFileSystemWithFSO
     Assert.assertEquals(valueGreaterBatchSize, fileStatuses.length);
   }
 
+  @Disabled
   @Test
   public void testLeaseRecoverable() throws Exception {
     // Create a file
