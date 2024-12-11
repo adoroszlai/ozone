2a258b14d9314fd1e4464b6e319d623261ddd337
Doroszlai, Attila <adoroszlai@apache.org>
2024-12-11 08:17:37 +0100

    HDDS-11909. Intermittent timeout building Hadoop in s3a test

--- hadoop-ozone/dist/src/main/compose/common/s3a-test.sh
+++ hadoop-ozone/dist/src/main/compose/common/s3a-test.sh
@@ -24,6 +24,8 @@ if [[ -z ${HADOOP_AWS_DIR} ]] || [[ ! -e ${HADOOP_AWS_DIR} ]]; then
   exit
 fi
 
+MAVEN_OPTIONS="--fail-never ${MAVEN_OPTIONS:-}"
+
 # shellcheck source=/dev/null
 source "$COMPOSE_DIR/../testlib.sh"
 
@@ -98,7 +100,7 @@ EOF
   # - ITestS3AContractDistCp: HDDS-10616
   # - ITestS3AContractMkdirWithCreatePerf: HDDS-11662
   # - ITestS3AContractRename: HDDS-10665
-  mvn -B -V --fail-never --no-transfer-progress \
+  mvn $MAVEN_OPTIONS \
     -Dtest='ITestS3AContract*, ITestS3ACommitterMRJob, !ITestS3AContractBulkDelete, !ITestS3AContractCreate#testOverwrite*EmptyDirectory[*], !ITestS3AContractDistCp, !ITestS3AContractMkdirWithCreatePerf, !ITestS3AContractRename' \
     clean test
 
