9f186b5933b99107e004edb19f35bf8514cfbb86
Doroszlai, Attila <adoroszlai@apache.org>
2025-01-27 08:45:32 +0100

    TEMP only build+kubernetes

--- hadoop-ozone/dist/src/main/k8s/examples/testlib.sh
+++ hadoop-ozone/dist/src/main/k8s/examples/testlib.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${SCRIPT_DIR}/../../smoketest/testlib.sh"
 
